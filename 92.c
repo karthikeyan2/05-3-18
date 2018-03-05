39641feff23d9a6d502582fcae4942b9310a38f2
karthikeyan2 <35687514+karthikeyan2@users.noreply.github.com>
2018-03-05 15:40:47 +0530

    92.c

---
+++ 92.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+void main(void) 
+{
+	int n,a,sum=0,i;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a);
+		sum=sum+a;
+	}
+	printf("%d",sum);
+	
+	return 0;
+}
